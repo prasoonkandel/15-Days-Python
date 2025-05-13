864e16ffe5af79e1745a2b3d2fbf6cf365767ab4
Prasoon202 <prasoonkandel1029@gmail.com>
2025-05-13 21:45:25 +0545

    first commit

---
+++ robot.py
@@ -0,0 +1,10 @@
+import emoji
+import pyttsx3
+import time
+e = emoji.emojize
+print(e('I will speak what you write!:wave::heart: ',  language='alias'))
+time.sleep(1)
+text = input("\n Enter text :")
+engine = pyttsx3.init()
+engine.say(text)
+engine.runAndWait()
