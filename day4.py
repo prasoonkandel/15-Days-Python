b1dcdc96899eccf9c437e091391595d6d79b2f72
Prasoon202 <prasoonkandel1029@gmail.com>
2025-05-16 23:05:11 +0545

    done

---
+++ day4.py
@@ -0,0 +1,17 @@
+import time
+import random
+gap = time.sleep
+#head or tails
+print("This is a simple head or tails game")
+gap(1)
+user_side = input("Heads or tails (h/t):")
+comp_side = random.randint(0 ,1)
+if (user_side=="h" ):
+    user_side = int(0)
+else:
+      user_side = int(1)
+
+if (user_side == comp_side):
+     print("You won")
+else:
+     print("You lost")     
