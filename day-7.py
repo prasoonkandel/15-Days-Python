7d0a9fc83e872702efb0381aceaa0ae34c7a2be0
Prasoon202 <prasoonkandel1029@gmail.com>
2025-05-20 21:51:47 +0545

    done

---
+++ day-7.py
@@ -0,0 +1,31 @@
+#This a qr code generator
+#install this module , pip install "qrcode[pil]"
+import qrcode
+import time
+gap = time.sleep
+print("QR code generator 🛠️💻")
+gap(0.5)
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr_data = input("Enter the text or URL for the QR Code: ")
+
+qr.add_data(qr_data)
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+print("Genarating...", end='', flush=True)
+gap(2)
+print(f"\rdone         \r")
+
+filename = input("Enter file name:")
+print("Saving...", end='', flush=True)
+gap(1)
+print(f"\rdone         \r")
+img.save(filename)
+
+print(f"✅ QR Code saved as {filename}")
+img.show()
