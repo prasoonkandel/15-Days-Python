94875c0fc07b27ef560b9cef365cb23820f503a1
Prasoon202 <prasoonkandel1029@gmail.com>
2025-05-21 22:26:00 +0545

    done

--- day-8.py
+++ day-8.py
@@ -1,12 +1,22 @@
 import random
 
 def generate(len):
-    all_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&()*@"
+    all_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&*@"
     password = ""
     for i in range(len):
         password += random.choice(all_chars)
     return password
 
 len = int(input("Enter the length of the password: "))
+less_length = False
+if len < 6:
+    less_length=True
+    while less_length==True:
+        print("Please at least 6 characters")
+        len = int(input("Enter the length of the password: "))
+        if len >= 6:
+            break
+else:
+    less_length=False
 password = generate(len)
 print(f"Your password is: {password}")
