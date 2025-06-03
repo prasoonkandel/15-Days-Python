6ec73520b35751c83a4192123dd602ced9307029
Prasoon202 <prasoonkandel1029@gmail.com>
2025-06-03 22:04:45 +0545

    done

---
+++ day-10.py
@@ -0,0 +1,20 @@
+import datetime
+import pyfiglet
+import time
+
+gap = time.sleep
+run = True
+
+while run:
+    now = datetime.datetime.now()
+    hour = now.hour
+    minute = now.minute
+    second = now.second
+    
+    current_time = f"{hour:02}:{minute:02}:{second:02}"
+    
+    print("\033c", end="")  
+    print(f"\nCurrent Time:\n")
+    print(pyfiglet.figlet_format(current_time))
+    
+    gap(1)
